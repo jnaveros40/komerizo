92fa603c8fde0ff5ee9288c0735695eeb2a83044
jnaveros40 <jnaveros40@gmail.com>
2026-03-24 11:27:02 -0500

    feat: remove SecretarioComunas and SecretarioRoles pages as they are under development

--- src/app/secretario/comunas/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SecretarioComunas() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Gestión de Comunas</h1>
-      <p>En desarrollo...</p>
-    </div>
-  )
-}
--- src/app/secretario/roles/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SecretarioRoles() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Gestión de Roles</h1>
-      <p>En desarrollo...</p>
-    </div>
-  )
-}
